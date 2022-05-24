342d47ff16fbbfa156eb29db929b3a01f6ea6ebd
yuto1108 <yuto-1108@au.com>
2022-05-24 18:28:38 +0900

    commit

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -18,7 +18,7 @@ function post (){
     const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
-    XHR.open("POST", "/posts", true);
+    XHR.open("POST", "/postss", true);
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
